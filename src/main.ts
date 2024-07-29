4286c5034138fd1263baa09b81095ebde91fbcdd
yuki <mark2peace@gmail.com>
2024-07-29 14:12:42 +0900

    add:fontawesomeセルフホストで使えるようにした

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import './assets/fontawesome/css/fontawesome.css';
+import './assets/fontawesome/css/brands.css';
+import './assets/fontawesome/css/solid.css';
 
 createApp(App).mount("#app");
