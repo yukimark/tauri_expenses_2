8b0817a7321dfb14ffd361016c1be8008998854f
yuki <mark2peace@gmail.com>
2024-07-30 13:24:05 +0900

    add:router追加

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,8 @@ import App from "./App.vue";
 import './assets/fontawesome/css/fontawesome.css';
 import './assets/fontawesome/css/brands.css';
 import './assets/fontawesome/css/solid.css';
+import { router } from './router';
 
-createApp(App).mount("#app");
+createApp(App)
+  .use(router)
+  .mount("#app");
