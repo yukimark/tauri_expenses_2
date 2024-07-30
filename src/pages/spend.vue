eed38ee9c60f792fb728466da7b16d8472baccb5
yuki <mark2peace@gmail.com>
2024-07-31 03:59:38 +0900

    add:日付入力欄作成(初期値入れるまで)

--- src/pages/spend.vue
+++ src/pages/spend.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
   import { ref, onMounted } from 'vue';
   import { useDatabaseStore } from '../stores/databaseStore';
+  import { Category, Categories, Spend } from '../types.ts';
+
+  let today: Date = new Date();
+  let formattedDate =`${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
 
   const databaseStore = useDatabaseStore();
-  const data = ref<any>(null);
+  const categoryAll = ref<Category[]>([]);
+  const inputDate = ref<String>(formattedDate);
 
+  
   onMounted(async () => {
     try {
-      data.value = await databaseStore.selectQuery('SELECT * FROM categories;');
-      console.log(data.value);
+      const result: Categories<Category> = await databaseStore.selectQuery('SELECT id, category FROM categories order by id asc;');
+      categoryAll.value = result;
+      console.log(categoryAll.value);
     } catch (error) {
       console.error('Query error', error);
     }
@@ -19,7 +26,21 @@
   <div class="title"><h1>お小遣い帳</h1></div>
   <form>
     <div>
-
+      <div class="input-date">
+        <VueDatePicker
+          v-model="inputDate"
+          format="yyyy/MM/dd"
+          model-type="yyyy-MM-dd"
+          locale="ja"
+          week-start="0"
+          :enable-time-picker="false"
+          auto-apply
+          :month-change-on-scroll="false"
+          required
+          :max-date="new Date()"
+          />
+        {{ inputDate }}
+      </div>
     </div>
   </form>
 </template>
@@ -32,5 +53,9 @@
 
   .title {
     padding-top: 20px;
+  }
+
+  .input-date {
+    width: 200px;
   }
 </style>
