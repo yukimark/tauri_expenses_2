4371f64a80556b1aeda92132751c174690d0c4b9
yuki <mark2peace@gmail.com>
2024-07-31 00:15:18 +0900

    add:spend.vue追加途中

--- src/pages/spend.vue
+++ src/pages/spend.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+  import { useDatabaseStore } from '../stores/databaseStore';
 
+  const databaseStore = useDatabaseStore();
+  const data = ref<any>(null);
+
+  onMounted(async () => {
+    try {
+      data.value = await databaseStore.selectQuery('SELECT * FROM categories;');
+      console.log(data.value);
+    } catch (error) {
+      console.error('Query error', error);
+    }
+  })
 </script>
 
 <template>
-  <p>spend</p>
+  <div class="title"><h1>お小遣い帳</h1></div>
+  <form>
+    <div>
+
+    </div>
+  </form>
 </template>
 
 <style scoped>
