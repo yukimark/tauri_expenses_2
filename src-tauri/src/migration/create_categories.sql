055779caffe22f2bce1d70f17ef275decc59c168
yuki <mark2peace@gmail.com>
2024-08-08 03:12:42 +0900

    fix:マイグレーション修正

--- src-tauri/src/migration/create_categories.sql
+++ src-tauri/src/migration/create_categories.sql
@@ -1,7 +1,7 @@
 CREATE TABLE categories (
   id                 INTEGER PRIMARY KEY,
   category           TEXT NOT NULL UNIQUE,
-  initial_flag       TEXT NOT NULL DEFAULT '1',
+  initial_flag       INTEGER NOT NULL DEFAULT 1,
   spend_target_value INTEGER NOT NULL DEFAULT 0,
   created_at         TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at         TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
