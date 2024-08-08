946958e640f798b3eecc9c111c5abe83619017c4
yuki <mark2peace@gmail.com>
2024-08-09 00:56:39 +0900

    add:集計目標値の更新機能実装

--- src/pages/config.vue
+++ src/pages/config.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import MultipleChoiceMenu from '../components/multipleChoiceMenu.vue'
-import { MultipleChoiceMenuParams, CreateCategory, ModalParams, UpdateDeleteCategory } from '../types'
+import { MultipleChoiceMenuParams, CreateCategory, ModalParams, UpdateDeleteCategory, GetUpdateProfile } from '../types'
 import { ref } from 'vue'
 import { useDatabaseStore } from '../stores/databaseStore'
 import { useCategoryStore } from '../stores/categoryStore'
-// import { useProfileStore } from '../stores/profileStore'
+import { useProfileStore } from '../stores/profileStore'
 import Modal from '../components/modal.vue'
 
 const databaseStore = useDatabaseStore()
 const categoryStore = useCategoryStore()
+const profileStore = useProfileStore()
 
 const multipleChoiceMenuParams: MultipleChoiceMenuParams[] = [
   { id: 1, value: '項目の追加' },
   { id: 2, value: '項目の編集･削除' },
+  { id: 3, value: '集計目標値の設定' },
 ]
 
 const categoryMenuChoice = ref<number>(1)
@@ -38,6 +40,12 @@ const modalParams = ref<ModalParams>({
 })
 
 const usedCategory = ref<boolean>(true)
+
+const updateProfile = ref<GetUpdateProfile>({
+  target_value_total_price: profileStore.profile[0].target_value_total_price,
+  target_value_fixed_cost: profileStore.profile[0].target_value_fixed_cost,
+  target_value_deferred_pay: profileStore.profile[0].target_value_deferred_pay,
+})
 
 const setModalParams = ({
   cssClass,
@@ -80,6 +88,11 @@ const categoryMenuToggle = async (index: number) => {
     category: '',
     spend_target_value: 0,
     initial_flag: true,
+  }
+  updateProfile.value = {
+    target_value_total_price: profileStore.profile[0].target_value_total_price,
+    target_value_fixed_cost: profileStore.profile[0].target_value_fixed_cost,
+    target_value_deferred_pay: profileStore.profile[0].target_value_deferred_pay,
   }
 }
 
@@ -135,6 +148,23 @@ const submitFormDeleteCategory = async () => {
   }
 }
 
+const submitFormUpdateProfile = async () => {
+  const value = updateProfile.value
+  try {
+    await databaseStore.updateProfile([value.target_value_total_price, value.target_value_fixed_cost, value.target_value_deferred_pay])
+    await profileStore.set(await databaseStore.getProfile())
+    setModalParams({ cssClass: 'success', message: '集計目標値の更新に成功しました。' })
+  } catch (error) {
+    console.error(error)
+    setModalParams({ cssClass: 'error', message: '集計目標値の更新に失敗しました。' })
+  }
+  updateProfile.value = {
+    target_value_total_price: profileStore.profile[0].target_value_total_price,
+    target_value_fixed_cost: profileStore.profile[0].target_value_fixed_cost,
+    target_value_deferred_pay: profileStore.profile[0].target_value_deferred_pay,
+  }
+}
+
 const formDisplaySetParams = async () => {
   const result = categoryStore.category.find((item) => item.id === formDataUpdateDeleteCategory.value.id)!
   const data = await databaseStore.usedCategory(result.id)
@@ -183,19 +213,40 @@ const formDisplaySetParams = async () => {
         <div v-if="formDataUpdateDeleteCategory.id" style="display: flex">
           <div v-if="!formDataUpdateDeleteCategory.initial_flag && !usedCategory" class="form-contents">
             <label for="input-category">項目</label>
-            <input type="text" v-model="formDataUpdateDeleteCategory.category" />
+            <input type="text" v-model="formDataUpdateDeleteCategory.category" id="input-category" />
           </div>
           <div v-else class="form-contents">
             <label for="input-category">項目</label>
-            <input type="text" v-model="formDataUpdateDeleteCategory.category" readonly class="read-only" />
+            <input type="text" v-model="formDataUpdateDeleteCategory.category" readonly class="read-only" id="input-category" />
           </div>
           <div class="form-contents">
             <label for="input-spend-target-value">目標値</label>
-            <input type="number" v-model="formDataUpdateDeleteCategory.spend_target_value" />
+            <input type="number" v-model="formDataUpdateDeleteCategory.spend_target_value" id="input-spend-target-value" />
           </div>
           <button @click="submitFormUpdateCategory" type="button">更新</button>
           <button v-if="!formDataUpdateDeleteCategory.initial_flag && !usedCategory" @click="submitFormDeleteCategory" type="button">削除</button>
         </div>
+      </div>
+    </form>
+  </div>
+  <div v-if="categoryMenuChoice === 3">
+    <form style="height: 180px">
+      <div class="form-row" style="height: 80px">
+        <div class="form-contents">
+          <label for="input-total-price">合計の目標</label>
+          <input type="number" v-model="updateProfile.target_value_total_price" id="input-total-price" />
+        </div>
+        <div class="form-contents">
+          <label for="input-fixed-cost">変動費の目標</label>
+          <input type="number" v-model="updateProfile.target_value_fixed_cost" id="input-fixed-cost" />
+        </div>
+        <div class="form-contents">
+          <label for="input-deferred-pay">後払いの目標</label>
+          <input type="number" v-model="updateProfile.target_value_deferred_pay" id="input-deferred-pay" />
+        </div>
+      </div>
+      <div class="form-row mb-20" style="justify-content: flex-end; margin-right: 30px">
+        <button @click="submitFormUpdateProfile" type="button" style="height: 40px">更新</button>
       </div>
     </form>
   </div>
