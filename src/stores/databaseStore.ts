3840bb7e35c9817207e55a23143803212e1af0bc
yuki <mark2peace@gmail.com>
2024-08-08 13:08:37 +0900

    fix:dbクエリに終了の;なかったので追記

--- src/stores/databaseStore.ts
+++ src/stores/databaseStore.ts
@@ -52,7 +52,7 @@ export const useDatabaseStore = defineStore('database', () => {
     if (!db.value) {
       throw new Error('Database is not connected')
     }
-    db.value.execute('INSERT into spends (date, category_id, price, fixed_cost, deferred_pay, memo) VALUES ($1, $2, $3, $4, $5, $6)', params)
+    db.value.execute('INSERT into spends (date, category_id, price, fixed_cost, deferred_pay, memo) VALUES ($1, $2, $3, $4, $5, $6);', params)
   }
 
   const deleteSpendsMatchId = async (params: number[]) => {
@@ -75,7 +75,7 @@ export const useDatabaseStore = defineStore('database', () => {
       throw new Error('Database is not connected')
     }
     params.push(0)
-    db.value.execute('INSERT into categories (category, spend_target_value, initial_flag) VALUES ($1, $2, $3)', params)
+    db.value.execute('INSERT into categories (category, spend_target_value, initial_flag) VALUES ($1, $2, $3);', params)
   }
 
   const usedCategory = async (id: number): Promise<[{ exists_flag: number }]> => {
