10b41ec679fdc844d237d474166659193469d433
yuki <mark2peace@gmail.com>
2024-08-23 03:46:08 +0900

    fix:タイムスタンプの型の可読性向上、updated_atに更新した時間が入るように修正 close #51

--- src-tauri/src/migration/create_profiles.sql
+++ src-tauri/src/migration/create_profiles.sql
@@ -3,8 +3,15 @@ CREATE TABLE profiles (
   target_value_total_price  INTEGER NOT NULL DEFAULT 0,
   target_value_fixed_cost   INTEGER NOT NULL DEFAULT 0,
   target_value_deferred_pay INTEGER NOT NULL DEFAULT 0,
-  created_at                TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at                TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+  created_at                TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at                DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO profiles DEFAULT VALUES;
+
+CREATE TRIGGER update_profiles_timestamp
+AFTER UPDATE ON profiles
+FOR EACH ROW
+BEGIN
+  UPDATE profiles SET updated_at = CURRENT_TIMESTAMP WHERE id = OLD.id;
+END;
--- src-tauri/src/migration/create_spends.sql
+++ src-tauri/src/migration/create_spends.sql
@@ -6,7 +6,14 @@ CREATE TABLE spends (
   fixed_cost   INTEGER NOT NULL,
   deferred_pay INTEGER NOT NULL,
   memo         TEXT,
-  created_at   TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at   TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (category_id) REFERENCES categories(id)
-)
+);
+
+CREATE TRIGGER update_spends_timestamp
+AFTER UPDATE ON spends
+FOR EACH ROW
+BEGIN
+  UPDATE spends SET updated_at = CURRENT_TIMESTAMP WHERE id = OLD.id;
+END;
